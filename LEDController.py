6e0c9631471aec61c5824b14be334240d55f6efc
mdczaplicki <mdczaplicki@gmail.com>
2016-02-10 19:00:40 +0100

    Sending colors

--- LEDController.py
+++ LEDController.py
@@ -42,7 +42,11 @@ class AThread(QtCore.QThread):
 # noinspection PyAttributeOutsideInit
 class UiLEDController(object):
     def __init__(self):
-        self.worker = AThread()
+        self.ser = serial.Serial()
+        self.ser.port = PORT
+        self.ser.baudrate = BAUDRATE
+        self.ser.bytesize = BYTESIZE
+        # self.worker = AThread()
 
     def setup_ui(self, led_controller):
         led_controller.setObjectName("led_controller")
@@ -57,10 +61,35 @@ class UiLEDController(object):
         led_controller.setMaximumSize(QtCore.QSize(230, 160))
         self.central_widget = QtWidgets.QWidget(led_controller)
         self.central_widget.setObjectName("central_widget")
+        self.mode_box = QtWidgets.QComboBox(self.central_widget)
+        self.mode_box.setGeometry(QtCore.QRect(10, 10, 211, 22))
+        self.mode_box.setObjectName("mode_box")
+        self.mode_box.addItem("")
+        self.mode_box.addItem("")
+        self.mode_box.addItem("")
+# region Sliders
         self.bright_slider = QtWidgets.QSlider(self.central_widget)
         self.bright_slider.setGeometry(QtCore.QRect(70, 40, 150, 20))
         self.bright_slider.setOrientation(QtCore.Qt.Horizontal)
+        self.bright_slider.setMaximum(255)
         self.bright_slider.setObjectName("bright_slider")
+        self.red_slider = QtWidgets.QSlider(self.central_widget)
+        self.red_slider.setGeometry(QtCore.QRect(70, 70, 150, 20))
+        self.red_slider.setOrientation(QtCore.Qt.Horizontal)
+        self.red_slider.setMaximum(254)
+        self.red_slider.setObjectName("red_slider")
+        self.green_slider = QtWidgets.QSlider(self.central_widget)
+        self.green_slider.setGeometry(QtCore.QRect(70, 100, 150, 20))
+        self.green_slider.setOrientation(QtCore.Qt.Horizontal)
+        self.green_slider.setMaximum(254)
+        self.green_slider.setObjectName("green_slider")
+        self.blue_slider = QtWidgets.QSlider(self.central_widget)
+        self.blue_slider.setGeometry(QtCore.QRect(70, 130, 150, 20))
+        self.blue_slider.setOrientation(QtCore.Qt.Horizontal)
+        self.blue_slider.setMaximum(254)
+        self.blue_slider.setObjectName("blue_slider")
+# endregion
+# region Labels
         self.bright_label = QtWidgets.QLabel(self.central_widget)
         self.bright_label.setGeometry(QtCore.QRect(10, 40, 55, 20))
         self.bright_label.setLayoutDirection(QtCore.Qt.LeftToRight)
@@ -76,29 +105,12 @@ class UiLEDController(object):
         self.green_label.setLayoutDirection(QtCore.Qt.LeftToRight)
         self.green_label.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter)
         self.green_label.setObjectName("green_label")
-        self.mode_box = QtWidgets.QComboBox(self.central_widget)
-        self.mode_box.setGeometry(QtCore.QRect(10, 10, 211, 22))
-        self.mode_box.setObjectName("mode_box")
-        self.mode_box.addItem("")
-        self.mode_box.addItem("")
-        self.mode_box.addItem("")
         self.blue_label = QtWidgets.QLabel(self.central_widget)
         self.blue_label.setGeometry(QtCore.QRect(10, 130, 55, 20))
         self.blue_label.setLayoutDirection(QtCore.Qt.LeftToRight)
         self.blue_label.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter)
         self.blue_label.setObjectName("blue_label")
-        self.red_slider = QtWidgets.QSlider(self.central_widget)
-        self.red_slider.setGeometry(QtCore.QRect(70, 70, 150, 20))
-        self.red_slider.setOrientation(QtCore.Qt.Horizontal)
-        self.red_slider.setObjectName("red_slider")
-        self.green_slider = QtWidgets.QSlider(self.central_widget)
-        self.green_slider.setGeometry(QtCore.QRect(70, 100, 150, 20))
-        self.green_slider.setOrientation(QtCore.Qt.Horizontal)
-        self.green_slider.setObjectName("green_slider")
-        self.blue_slider = QtWidgets.QSlider(self.central_widget)
-        self.blue_slider.setGeometry(QtCore.QRect(70, 130, 150, 20))
-        self.blue_slider.setOrientation(QtCore.Qt.Horizontal)
-        self.blue_slider.setObjectName("blue_slider")
+# endregion
         self.pick_button = QtWidgets.QPushButton(self.central_widget)
         self.pick_button.setGeometry(QtCore.QRect(10, 130, 20, 20))
         self.pick_button.setText("")
@@ -109,11 +121,11 @@ class UiLEDController(object):
         self.retranslate_ui(led_controller)
         self.mode_box.setCurrentIndex(0)
         self.mode_box.currentIndexChanged['int'].connect(self.connect_ino)
-        '''self.blue_slider.sliderMoved['int'].connect()
-        self.green_slider.sliderMoved['int'].connect()
-        self.bright_slider.sliderMoved['int'].connect()
-        self.red_slider.sliderMoved['int'].connect()
-        self.pick_button.clicked.connect()'''
+        self.blue_slider.valueChanged.connect(self.change_color)
+        self.green_slider.valueChanged.connect(self.change_color)
+        self.bright_slider.valueChanged.connect(self.change_color)
+        self.red_slider.valueChanged.connect(self.change_color)
+        # self.pick_button.clicked.connect()
         QtCore.QMetaObject.connectSlotsByName(led_controller)
 
         self.tray = QtWidgets.QSystemTrayIcon(self.central_widget)
@@ -132,6 +144,16 @@ class UiLEDController(object):
         self.mode_box.setItemText(2, _translate("led_controller", "Automatic"))
         self.blue_label.setText(_translate("led_controller", "Blue"))
 
+    def __except__(self):
+        exc_type, exc_obj, tb = sys.exc_info()
+        f = tb.tb_frame
+        lineno = tb.tb_lineno
+        filename = f.f_code.co_filename
+        linecache.checkcache(filename)
+        line = linecache.getline(filename, lineno, f.f_globals)
+        self.__show_warning__("Error", "Exception in file:\n%s\nLine number: %i\n%s\nException type: %s" %
+                              (filename, lineno, line.strip(), exc_obj))
+
     @staticmethod
     def __show_warning__(title, text):
         message_box = QtWidgets.QMessageBox()
@@ -142,43 +164,40 @@ class UiLEDController(object):
     def connect_ino(self, selected):
         # TODO: clean this function
         try:
-            self.worker.start()
+            # self.worker.start()
             if not selected:
                 # OFF
                 if self.ser.isOpen():
+                    self.ser.write([0xff, 0, 0, 0])
                     self.ser.close()
             elif selected == 1:
                 # MANUAL
                 if not self.ser.isOpen():
                     self.ser.open()
+                time.sleep(1.7)
+                self.change_color(None)
             elif selected == 2:
                 # AUTO
                 if not self.ser.isOpen():
                     self.ser.open()
         except:
-            exc_type, exc_obj, tb = sys.exc_info()
-            f = tb.tb_frame
-            lineno = tb.tb_lineno
-            filename = f.f_code.co_filename
-            linecache.checkcache(filename)
-            line = linecache.getline(filename, lineno, f.f_globals)
-            self.__show_warning__("Error", "Exception in file:\n%s\nLine number: %i\n%s\nException type: %s" %
-                                  (filename, lineno, line.strip(), exc_obj))
+            self.__except__()
 
-    def change_color(self, value):
-        # TODO: I should send frames over here without thread
-        sender = self.central_widget.sender().objectName()
-        if sender == "red_slider":
-            pass
-        elif sender == "green_slider":
-            pass
-        elif sender == "blue_slider":
-            pass
+    def change_color(self, _):
+        try:
+            brightness = self.bright_slider.value()
+            red = int(self.red_slider.value() * brightness / 255)
+            green = int(self.green_slider.value() * brightness / 255)
+            blue = int(self.blue_slider.value() * brightness / 255)
+            print([0xff, red, green, blue])
+            if self.ser.isOpen():
+                self.ser.write([0xff, red, green, blue])
+        except:
+            self.__except__()
 
 
 if __name__ == "__main__":
     import sys
-
     app = QtWidgets.QApplication(sys.argv)
     LEDController = QtWidgets.QMainWindow()
     ui = UiLEDController()
